d7400df27350eb50e704716517807ee175b8817a
Emil Johansson <emil@stepout.se>
2026-09-03 16:07:03 +0200

    Updated to use dracula theme and added jk keybinding

---
+++ nvim/lua/plugins/dracula.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "maxmx03/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("dracula").setup({
+        transparent = true,
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
